13aa6030357e380796269a980ac320c1339ce342
Navyaarekuti <arekutinavya1@gmail.com>
2025-11-17 19:28:23 -0600

    Sample

--- src/main/java/org/launchcode/ProductService/controllers/SampleController.java
+++ src/main/java/org/launchcode/ProductService/controllers/SampleController.java
@@ -1,5 +1,21 @@
 package org.launchcode.ProductService.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SampleController {
+    @GetMapping("/sayHello")
+    public String sayHelloWorld()
+    {
+        return "Hello World";
+    }
+
+    @GetMapping("/sayBye/{name}")
+        public String sayBye(@PathVariable("name") String name)
+        {
+            return "Bye" + name;
+        }
 }
 
