34bd3ab469c3348977b2d7fa21ed26a0bddf2a91
Navyaarekuti <arekutinavya1@gmail.com>
2025-12-04 18:03:39 -0600

    testinf

---
+++ src/main/java/org/launchcode/ProductService/models/Product.java
@@ -0,0 +1,13 @@
+package org.launchcode.ProductService.models;
+
+import jdk.jfr.Category;
+
+public class Product {
+    private long id;
+    private String title;
+    private double price;
+    private String description;
+    private String image;
+    private Category category;
+
+}
